8db04ae6f2b9d9b4ceaec670a89a2f088368d21c
James Lindstorff <jlindstorff@gmail.com>
2021-12-19 07:27:54 +0100

    flags

--- reconsile.go
+++ reconsile.go
@@ -5,10 +5,19 @@ import (
 	"fmt"
 )
 
-var secret = flag.String("secret", "", "API secret to use for communicating with JW")
-
 func main() {
+	var secret = flag.String("secret", "", "API secret to use for communicating with JW")
+	var showIds = flag.Bool("id", false, "Include JW webhook id in out from list.")
+	var spec = flag.String("spec", "", "Path to the specification file.")
+	flag.Parse()
+
+	//flag.PrintDefaults()
+
+	fmt.Println("Arguments left", flag.NArg())
 
 	//args := os.Args
-	fmt.Println(secret)
+	fmt.Println("Specification file:", *spec)
+	fmt.Println("Show ids:", *showIds)
+	fmt.Println("Secret:", *secret)
+
 }
