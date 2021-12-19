1cde3afba00b9b92df168226fcdbed593c8c1b8b
James Lindstorff <jlindstorff@gmail.com>
2021-12-19 16:52:11 +0100

    read the spec file

--- reconsile.go
+++ reconsile.go
@@ -1,11 +1,21 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
 )
+
+// Hand the JSON used uppercase the suffixes would not be needed
+type WebhookDefinition struct {
+	Description string   `json:"description"`
+	Events      []string `json:"events"`
+	Site_ids    []string `json:"site_ids"`
+	Endpoint    string   `json:"endpoint"`
+}
 
 func usage() {
 	fmt.Printf("Usage: %s arguments <command> \n", os.Args[0])
@@ -47,6 +57,17 @@ func main() {
 		fmt.Printf("File %s not found\n", *spec)
 		os.Exit(1)
 	}
+
+	f, err := ioutil.ReadFile(*spec)
+	if err != nil {
+		fmt.Printf("Failed to read %s\n", *spec)
+		os.Exit(1)
+	}
+
+	var definitions map[string]WebhookDefinition
+	json.Unmarshal([]byte(f), &definitions)
+
+	fmt.Println(definitions)
 
 	fmt.Println("Specification file:", *spec)
 	fmt.Println("Show ids:", *showIds)
