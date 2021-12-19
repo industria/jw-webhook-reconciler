83a6a55f2f5362fcc13a543607a7e8c2f6101ef2
James Lindstorff <jlindstorff@gmail.com>
2021-12-19 09:20:51 +0100

    steps

--- reconsile.go
+++ reconsile.go
@@ -3,15 +3,42 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
+
+func usage() {
+	fmt.Printf("Usage: %s <command> arguments.. \n", os.Args[0])
+	fmt.Printf("  command : list, diff or apply\n\n")
+	flag.PrintDefaults()
+}
+
+func validCommand(cmd string) bool {
+	cmds := []string{"list", "diff", "apply"}
+	for _, s := range cmds {
+		if s == cmd {
+			return true
+		}
+	}
+	return false
+}
 
 func main() {
 	var secret = flag.String("secret", "", "API secret to use for communicating with JW")
 	var showIds = flag.Bool("id", false, "Include JW webhook id in out from list.")
 	var spec = flag.String("spec", "", "Path to the specification file.")
+	flag.Usage = usage
 	flag.Parse()
 
-	//flag.PrintDefaults()
+	if flag.NArg() == 0 {
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if !validCommand(flag.Arg(0)) {
+		fmt.Printf("Unknown command %s", flag.Arg(0))
+		flag.Usage()
+		os.Exit(1)
+	}
 
 	fmt.Println("Arguments left", flag.NArg())
 
