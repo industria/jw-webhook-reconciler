b20ce6b76bcd1f86fc924802face96f413656d49
James Lindstorff <james.lindstorff@jppol.dk>
2021-12-22 15:32:42 +0100

    start comparing definition and declaration

---
+++ declarations.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+)
+
+// Hand the JSON used uppercase the suffixes would not be needed
+type webhookDeclaration struct {
+	Description string   `json:"description"`
+	Events      []string `json:"events"`
+	Site_ids    []string `json:"site_ids"`
+	Endpoint    string   `json:"endpoint"`
+}
+
+type Declaration struct {
+	name        string
+	description string
+	events      []string
+	siteIds     []string
+	endpoint    string
+}
+
+func newDeclaration(name string, decl webhookDeclaration) *Declaration {
+	return &Declaration{name, decl.Description, decl.Events, decl.Site_ids, decl.Endpoint}
+}
+
+func Declarations(declarationFile string) ([]Declaration, error) {
+	var result []Declaration
+
+	f, err := ioutil.ReadFile(declarationFile)
+	if err != nil {
+		return result, fmt.Errorf("Failed to read %s", declarationFile)
+	}
+
+	var declarations map[string]webhookDeclaration
+	err = json.Unmarshal([]byte(f), &declarations)
+	if err != nil {
+		return result, fmt.Errorf("Failed to parse %s", declarationFile)
+	}
+
+	for name, decl := range declarations {
+		newDeclaration(name, decl)
+		result = append(result, *newDeclaration(name, decl))
+	}
+
+	return result, nil
+}
--- reconsile.go
+++ reconsile.go
@@ -1,21 +1,85 @@
 package main
 
 import (
-	"encoding/json"
 	"errors"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"os"
 	"text/tabwriter"
 )
 
-// Hand the JSON used uppercase the suffixes would not be needed
-type WebhookDeclaration struct {
-	Description string   `json:"description"`
-	Events      []string `json:"events"`
-	Site_ids    []string `json:"site_ids"`
-	Endpoint    string   `json:"endpoint"`
+type match struct {
+	declaration Declaration
+	definition  WebhookDefinition
+}
+
+type changeset struct {
+	create []Declaration
+	modify []match
+	delete []WebhookDefinition
+}
+
+func declarationInDefinitions(declaration Declaration, definitions []WebhookDefinition) bool {
+	for _, definition := range definitions {
+		if declaration.name == definition.MetaData.Name {
+			return true
+		}
+	}
+	return false
+}
+
+var DefinitionNotFound = errors.New("DefinitionNotFound")
+
+func findDefinitionFromDeclaration(declaration Declaration, definitions []WebhookDefinition) (match, error) {
+	for _, definition := range definitions {
+		if declaration.name == definition.MetaData.Name {
+			return match{declaration, definition}, nil
+		}
+	}
+	return match{}, DefinitionNotFound
+}
+
+func definitionInDeclarations(definition WebhookDefinition, declarations []Declaration) bool {
+	for _, declaration := range declarations {
+		if definition.MetaData.Name == declaration.name {
+			return true
+		}
+	}
+	return false
+}
+
+func different(match match) bool {
+	// TODO: compare
+	return false
+}
+
+func changeSet(declarations []Declaration, definitions []WebhookDefinition) *changeset {
+	var create []Declaration
+	var modify []match
+	var delete []WebhookDefinition
+
+	// Create declarations not found in definitions
+	for _, declaration := range declarations {
+		if !declarationInDefinitions(declaration, definitions) {
+			create = append(create, declaration)
+		}
+	}
+
+	// Modify if declaration is found in definitions and the attribute values differs
+	for _, declaration := range declarations {
+		match, err := findDefinitionFromDeclaration(declaration, definitions)
+		if nil == err && different(match) {
+			modify = append(modify, match)
+		}
+	}
+
+	// Delete definitions not found in declarations
+	for _, definition := range definitions {
+		if !definitionInDeclarations(definition, declarations) {
+			delete = append(delete, definition)
+		}
+	}
+	return &changeset{create, modify, delete}
 }
 
 func usage() {
@@ -36,7 +100,6 @@ func validCommand(cmd string) bool {
 
 func main() {
 	var secret = flag.String("secret", "", "API secret to use for communicating with JW")
-	var showIds = flag.Bool("id", false, "Include JW webhook id in out from list.")
 	var spec = flag.String("spec", "", "Path to the specification file.")
 	flag.Usage = usage
 	flag.Parse()
@@ -59,25 +122,18 @@ func main() {
 		os.Exit(1)
 	}
 
-	f, err := ioutil.ReadFile(*spec)
+	declarations, err := Declarations(*spec)
 	if err != nil {
-		fmt.Printf("Failed to read %s\n", *spec)
+		fmt.Printf("err: %v\n", err)
 		os.Exit(1)
 	}
-
-	var declarations map[string]WebhookDeclaration
-	json.Unmarshal([]byte(f), &declarations)
-	if nil != declarations {
-		fmt.Println("Spec read")
-		//fmt.Println(definitions)
-	}
+	fmt.Println("Declarations file:", declarations)
 
 	fmt.Println("Specification file:", *spec)
-	fmt.Println("Show ids:", *showIds)
 	fmt.Println("Secret:", *secret)
 
 	Setup(*secret)
-	res, err := WebhooksDefinitions()
+	definitions, err := WebhooksDefinitions()
 	if err != nil {
 		fmt.Printf("Failed to get the webhooks from JW service \n")
 		os.Exit(1)
@@ -88,13 +144,24 @@ func main() {
 		w := tabwriter.NewWriter(os.Stdout, 0, 0, padding, ' ', tabwriter.Debug)
 		fmt.Fprintf(w, "Id\tName\tURL\tSites\tEvents  \n")
 
-		for _, definition := range res {
+		for _, definition := range definitions {
 			fmt.Fprintf(w, "%s\t%s\t%s\t%s\t%s\t \n", definition.Id, definition.MetaData.Name, definition.MetaData.Url, definition.MetaData.SiteIds, definition.MetaData.Events)
 		}
 		w.Flush()
+	} else if cmd == "diff" {
+
+		changeset := changeSet(declarations, definitions)
+		fmt.Println("Create:")
+		fmt.Println(changeset.create)
+
+		fmt.Println("Modify:")
+		fmt.Println(changeset.modify)
+
+		fmt.Println("Delete:")
+		fmt.Println(changeset.delete)
+
 	} else {
 		fmt.Printf("Unknown command %s\n", cmd)
-
 	}
 
 }
