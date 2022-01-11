da36b49ece2663e6926d2c24b70dfee5e7f14369
James Lindstorff <james.lindstorff@jppol.dk>
2022-01-11 07:02:11 +0100

    Just let the command switch validate the command

--- reconsile.go
+++ reconsile.go
@@ -114,16 +114,6 @@ func usage() {
 	flag.PrintDefaults()
 }
 
-func validCommand(cmd string) bool {
-	cmds := []string{"list", "diff", "apply"}
-	for _, s := range cmds {
-		if s == cmd {
-			return true
-		}
-	}
-	return false
-}
-
 func main() {
 	var secret = flag.String("secret", "", "API secret to use for communicating with JW")
 	var spec = flag.String("spec", "", "Path to the specification file.")
@@ -137,11 +127,6 @@ func main() {
 	}
 
 	cmd := flag.Args()[0]
-	if !validCommand(cmd) {
-		fmt.Printf("Unknown command %s\n", cmd)
-		flag.Usage()
-		os.Exit(1)
-	}
 
 	_, err := os.Stat(*spec)
 	if errors.Is(err, os.ErrNotExist) {
@@ -204,5 +189,7 @@ func main() {
 		}
 	} else {
 		fmt.Printf("Unknown command %s\n", cmd)
+		flag.Usage()
+		os.Exit(1)
 	}
 }
